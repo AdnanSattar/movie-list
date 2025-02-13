653e106b5aeeaa4066ad1b72e045ace5b4f641e6
Adnan Sattar <adnansattar09@gmail.com>
2025-02-13 09:06:10 +0500

    Api.js update

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,4 +1,4 @@
-const API_KEY = "06abb5c4652c91c613841671c639f37e";
+const API_KEY = import.meta.env.VITE_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
